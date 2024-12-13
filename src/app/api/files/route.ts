18694c123e844e10401c0522acf8240982af97eb
Samuel Sobalvarro <dev.msobalvarro@gmail.com>
2024-12-13 10:55:24 -0600

    validation file extension

--- src/app/api/files/route.ts
+++ src/app/api/files/route.ts
@@ -7,23 +7,35 @@ const uploadDir = './uploads'
 
 export async function POST(request: NextRequest) {
   try {
+    // valdiate file
     const formData = await request.formData()
     const file = formData.get('file')
     if (!file || !(file instanceof File)) throw new Error('No files received')
 
+    // get buffer
+    const buffer = Buffer.from(await file.arrayBuffer())
+    const fileExt = path.extname(file.name).toLocaleLowerCase()
+
+    // allowed extensions
+    const allowedTypes = /jpeg|jpg|png/
+    const extname = allowedTypes.test(fileExt)
+    if (!extname) throw new Error('extension is not allowed')
+
+    const fileName = `${crypto.randomUUID()}${fileExt}`
+
+    // create folder if not exists
     if (!existsSync(uploadDir)) {
       mkdirSync(uploadDir, { recursive: true })
     }
 
-    const buffer = Buffer.from(await file.arrayBuffer())
-
+    // write file
     await writeFile(
       path.join(
-        process.cwd(), `${uploadDir}/${crypto.randomUUID()}${path.extname(file.name)}`),
+        process.cwd(), `${uploadDir}/${fileName}`),
       buffer
     )
 
-    return NextResponse.json({ success: true })
+    return NextResponse.json({ file: fileName })
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 })
   }
